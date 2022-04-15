ddf9ed98964e8f0e806f590b8d12fc9428745c5a
IvanArkhipov <redm4nredm4n@gmail.com>
2022-04-15 20:36:48 +0300

    lesson1

---
+++ Homework1/ValuesHolder.cs
@@ -0,0 +1,41 @@
+﻿using System.Text.Json;
+using System.IO;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using Homework1;
+
+namespace Homework1
+{
+    public class ValuesHolder
+    {
+        private ConcurrentDictionary<DateTime, WeatherValue> input;
+        public ValuesHolder()
+        {
+            input = new ConcurrentDictionary<DateTime, WeatherValue>();
+        }
+
+        public ICollection<WeatherValue> Get() => input.Values;
+
+        public WeatherValue Get(DateTime date) =>
+            input.TryGetValue(date, out var value) ? value : null;
+
+        public void Add(WeatherValue value)
+        {
+            input.TryAdd(value.Date, value);
+        }
+
+        public bool Update(DateTime date, float newValue)
+        {
+            if (!input.TryGetValue(date, out var item))
+                return false;
+            item.Temp = newValue;
+            return true;
+        }
+
+        public bool Remove(DateTime date)
+        {
+            return input.TryRemove(date, out _);
+        }
+    }
+}
---
+++ Homework1/WeatherValue.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Homework1
+{
+    public class WeatherValue
+    {
+        public DateTime Date { get; set; }
+        public float Temp { get; set; }
+    }
+}
