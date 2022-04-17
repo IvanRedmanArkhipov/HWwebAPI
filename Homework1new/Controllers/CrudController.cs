69fa509133c16454c514a3ba6926ce55f5879ac5
IvanArkhipov <redm4nredm4n@gmail.com>
2022-04-17 17:14:18 +0300

    lesson1 (new)

---
+++ Homework1new/Controllers/CrudController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Homework1new.Controllers
+{
+    public class CrudController : Controller
+    {
+        private readonly ValuesHolder _holder;
+        public CrudController(ValuesHolder holder)
+        {
+            _holder = holder;
+        }
+        [HttpPost("create")]
+        public IActionResult Create([FromQuery] string input)
+        {
+            _holder.Add(input);
+            return Ok();
+        }
+        [HttpGet("read")]
+        public IActionResult Read()
+        {
+            return Ok(_holder.Get());
+        }
+        [HttpPut("update")]
+        public IActionResult Update([FromQuery] string stringsToUpdate,
+        [FromQuery] string newValue)
+        {
+            _holder.Update(stringsToUpdate, newValue);
+            return Ok();
+        }
+        [HttpDelete("delete")]
+        public IActionResult Delete([FromQuery] string stringsToDelete)
+        {
+            _holder.Delete(stringsToDelete);
+            return Ok();
+        }        
+    }
+}
---
+++ Homework1new/ValuesHolder.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+namespace Homework1new
+{
+    public class ValuesHolder
+    {
+        public List<string> Values = new List<string>();
+        public void Add(string input)
+        {
+            Values.Add(input);
+        }
+
+        public string Get()
+        {
+            foreach(string i in Values)
+            {
+                return i;
+            }
+            return "";
+        }
+
+        public void Update(string stringsToUpdate, string newValue)
+        {
+            for(int i = 0; i < 1; i++ )
+            {
+                if ( Values[i] == stringsToUpdate )
+                {
+                    Values[i] = newValue;
+                }
+            }
+        }
+        public void Delete(string stringsToDelete)
+        {
+            Values.Remove(stringsToDelete);
+        }
+    }
+}
